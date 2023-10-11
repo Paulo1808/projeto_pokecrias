bebe860afaad9c375f26ddd08526436a7d59ec79
vZkzin <paulovinicius1808@icloud.com>
2023-10-11 17:34:38 -0300

    principais
    criado a struct mochila

---
+++ CodigosPrincipais/principal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+    int pokemon1;
+    int pokemon2;
+    int pokemon3;
+    int pokemon4;
+    int pokemon5;
+    int pokemon6;
+
+} mochila;
+
+int	main()
+{
+    printf("Oi fml, pokecrias.");
+
+
+    
+    return 0;
+}//main
+
