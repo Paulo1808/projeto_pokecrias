0e6f62e568025ffbfd211ef0e771756097acb7a9
sartori_mi <145930163+sartorimi@users.noreply.github.com>
2023-10-11 17:34:35 -0300

    STRUCT POKEDEX
    struct com as informações da pokedex

---
+++ CodigosPrincipais/principal.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+
+typedef struct{
+    int numero;
+    char nome[31], tipo1[31], tipo2[31];
+    int total, ataque, defesa, ataque_especial, defesa_especial;
+    int velocidade, geracao;
+    bool lendario;
+    char cor;
+    int taxa_de_captura, prox_evolucao, pre_evolucao;
+    float altura, peso;
+}Pokedex; //definindo struct
