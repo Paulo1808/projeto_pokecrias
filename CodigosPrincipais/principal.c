09ce10057915b7938c8e7810da25eb45cbb4e654
sartori_mi <145930163+sartorimi@users.noreply.github.com>
2023-11-02 23:01:20 -0300

    tirei todas as declaraçoes implicitas de funcoes

--- CodigosPrincipais/principal.c
+++ CodigosPrincipais/principal.c
@@ -35,57 +35,6 @@ typedef struct{
     int *pokemonCapturado;
 }Colecao; //struct coleçao
 
-//funçao do menu inicial
-void exibeMenu()
-{
-    char* nickname = NULL;
-    int tam = 21;
-
-    nickname = (char*) malloc (tam * sizeof(char)); //aloca dinamicamente o tamanho do nome inserido
-    int opcao;
-
-    printf("Bem vindo ao Menu inicial!!!\n");
-    printf("Primeiramente, me diga seu nickname para que possamos continuar:\n");
-    setbuf(stdin, NULL);
-    fgets(nickname, 20, stdin);
-    nickname[strcspn(nickname, "\n")] = '\0';
-    setbuf(stdin, NULL); //recebe o nome inserido
-
-    printf("Bom, vamos lá %s!\n", nickname);
-    printf("Escolha uma opção para iniciar:\n");
-    printf("1 - Novo Jogo\n");
-    printf("2 - Carregar Jogo\n");
-    printf("3 - Mecanicas do Jogo\n"); //menu do jogo
-    printf("4 - Fechar o jogo\n");
-
-    scanf("%i", &opcao);
-
-    switch (opcao) //executa a opçao escolhida
-    {
-        case 1:
-            printf("Envia para a funçao de inicio de jogo\n");
-            PesquisaTipoGen();
-            break;
-        case 2:
-            printf("Carrega um arquivo do jogo ja existente\n");
-
-            break;
-        case 3:
-            printf("Exibe as mecanicas utilizadas no jogo\n");
-
-            break;
-        case 4:
-            printf("Fechando o jogo...\n");
-
-            break;
-        
-        default:
-            printf("Opção invalida...");
-
-            break;
-    }
-}//fim funçao do menu
-
 void CriaVetorPokedex(Pokemon pokedexGeral[])
 {
     FILE *ArquivoPokedex = fopen("pokedex.csv", "r"); //abre o arquivo da pokedex
@@ -124,7 +73,6 @@ void CriaVetorPokedex(Pokemon pokedexGeral[])
     fclose(ArquivoPokedex); //fecha o arquivo
 }//criaVetorPokedex
 
-//funçao pra pesquisar o tipo
 void PesquisaTipoGen()
 {    
     //variaveis 
@@ -256,6 +204,60 @@ void PesquisaTipoGen()
             break;
     }//switch
 }//fim funçao de pesquisa
+
+//funçao do menu inicial
+void exibeMenu()
+{
+    char* nickname = NULL;
+    int tam = 21;
+
+    nickname = (char*) malloc (tam * sizeof(char)); //aloca dinamicamente o tamanho do nome inserido
+    int opcao;
+
+    printf("Bem vindo ao Menu inicial!!!\n");
+    printf("Primeiramente, me diga seu nickname para que possamos continuar:\n");
+    setbuf(stdin, NULL);
+    fgets(nickname, 20, stdin);
+    nickname[strcspn(nickname, "\n")] = '\0';
+    setbuf(stdin, NULL); //recebe o nome inserido
+
+    printf("Bom, vamos lá %s!\n", nickname);
+    printf("Escolha uma opção para iniciar:\n");
+    printf("1 - Novo Jogo\n");
+    printf("2 - Carregar Jogo\n");
+    printf("3 - Mecanicas do Jogo\n"); //menu do jogo
+    printf("4 - Fechar o jogo\n");
+
+    scanf("%i", &opcao);
+
+    switch (opcao) //executa a opçao escolhida
+    {
+        case 1:
+            printf("Envia para a funçao de inicio de jogo\n");
+            PesquisaTipoGen();
+            break;
+        case 2:
+            printf("Carrega um arquivo do jogo ja existente\n");
+
+            break;
+        case 3:
+            printf("Exibe as mecanicas utilizadas no jogo\n");
+
+            break;
+        case 4:
+            printf("Fechando o jogo...\n");
+
+            break;
+        
+        default:
+            printf("Opção invalida...");
+
+            break;
+    }
+}//fim funçao do menu
+
+//funçao pra pesquisar o tipo
+
 
 //funçao de salvamento
 void salvamentoNoHD()
